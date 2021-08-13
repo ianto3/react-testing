c1db9a80519fa8719eeb3b3d763b8199827e8af4
ianto3 <33053514+ianto3@users.noreply.github.com>
2021-08-13 13:27:50 +0200

    Trying out test-driven development on Counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import "./Counter.css";
 
 const Counter = () => {
+    const [counterValue, setCounterValue] = useState(0);
+    const [inputValue, setInputValue] = useState(1);
+
     return (
         <div>
             <h1 data-testid="header">Counter</h1>
+            <h2 data-testid="counter">{counterValue}</h2>
+            <button data-testid="sub-btn">-</button>
+            <input
+                className="text-center"
+                type="number"
+                data-testid="input"
+                defaultValue={inputValue}
+            ></input>
+            <button data-testid="add-btn">+</button>
         </div>
     )
 }
--- src/components/Counter/__test__/Counter.test.js
+++ src/components/Counter/__test__/Counter.test.js
@@ -14,3 +14,31 @@ test("Test if header renders correctly", () => {
 
     expect(headerEl.textContent).toBe("Counter");
 });
+
+test("Counter intializes with text of 0", () => {
+    const { getByTestId } = render(<Counter />);
+    const counterEl = getByTestId("counter");
+
+    expect(counterEl.textContent).toBe("0");
+})
+
+test("Input initially contains the value of 1", () => {
+    const { getByTestId } = render(<Counter />);
+    const inputEl = getByTestId("input");
+
+    expect(inputEl.value).toBe("1");
+})
+
+test("Add button renders with + sign", () => {
+    const { getByTestId } = render(<Counter />);
+    const addBtn = getByTestId("add-btn");
+
+    expect(addBtn.textContent).toBe("+");
+})
+
+test("Subtract button renders with - sign", () => {
+    const { getByTestId } = render(<Counter />);
+    const subtractBtn = getByTestId("sub-btn");
+
+    expect(subtractBtn.textContent).toBe("-");
+})
